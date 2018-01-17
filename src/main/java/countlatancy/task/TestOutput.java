0de6bfdc66cad02529d197b262103466e96ee835
maoyancan <maoyancan@qq.com>
2018-01-16 20:13:37 -0800

    bugfix

--- src/main/java/countlatancy/task/TestOutput.java
+++ src/main/java/countlatancy/task/TestOutput.java
@@ -35,7 +35,7 @@ import org.apache.samza.task.TaskCoordinator;
 import org.apache.samza.task.WindowableTask;
 import org.json.JSONObject;
 
-public class TestOutput implements StreamTask,WindowableTask,InitableTask{
+public class TestOutput implements StreamTask,InitableTask{
     Map<String,Integer> clickEvent = new HashMap<String,Integer>();
     Map<String,Integer> clickCount = new HashMap<String,Integer>();
     int total = 0;
@@ -47,8 +47,7 @@ public class TestOutput implements StreamTask,WindowableTask,InitableTask{
             TaskCoordinator coodinator) throws Exception {
         Object msg = envelope.getMessage();
         System.out.println(msg);
-        System.out.println(msg.value());
-        collector.send(new OutgoingMessageEnvelope(output_stream, msg));
+        collector.send(new OutgoingMessageEnvelope(stream, msg));
     }
 
     public void init(Config config, TaskContext task){
