f1db3da2f4611446f57ec6f66ecf31abc3e2035d
maoyancan <maoyancan@qq.com>
2018-03-27 18:09:07 -0700

    update SB

--- src/main/java/countlatancy/task/ShareSBApp.java
+++ src/main/java/countlatancy/task/ShareSBApp.java
@@ -544,7 +544,7 @@ public class ShareSBApp implements StreamApplication {
                     totalOrderPrice += entry.value().get(i).getOrderPrice() * entry.value().get(i).getOrderExecVol();
                 }
                 // avgPriceList.put(entry.key() , totalOrderPrice/totalOrderVol);
-                messageBuilder.append(entry.key()).append(":")).append(String.valueOf(totalOrderPrice/totalOrderVol)).append(";");
+                messageBuilder.append(entry.key()).append(":").append(String.valueOf(totalOrderPrice/totalOrderVol)).append(";");
             }
             return messageBuilder.toString();
         }
